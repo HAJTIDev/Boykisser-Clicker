a588a4991fa95a7de18e764e018c351294b8aaa5
Paul Montague <paul.montague@eraneos.com>
2024-06-12 02:37:43 +0200

    awda

--- boykisser.js
+++ boykisser.js
@@ -1,25 +1,37 @@
 
 var imageSources = "https://kayofeld.github.io/Cookie-Clicker-mod/"
 
+var grandmas = [
+	'alteredGrandma',
+	'alternateGrandma',
+	'antiGrandma',
+	'bankGrandma',
+	'brainyGrandma',
+	'bunnyGrandma',
+	'cloneGrandma',
+	'cosmicGrandma',
+	'emetaGrandma',
+	'elfGrandma',
+	'farmerGrandma',
+	'grandma',
+	'grandmasGrandma',
+	'luckyGrandma',
+	'metaGrandma',
+	'minerGrandma',
+	'rainbowGrandma',
+	'scriptGrandma',
+	'templeGrandma',
+	'transmutedGrandma',
+	'witchGrandma',
+	'workerGrandma'
+];
+
 Game.Loader.Replace('buildings.png',imageSources+'buildingss.png');
 Game.Loader.Replace('perfectCookie.png',imageSources+'boykissser.png');
 Game.Loader.Replace('cursor.png',imageSources+'cursor.png');
 Game.Loader.Replace('goldCookie.png',imageSources+'kawaiiCookie.png');
-Game.Loader.Replace('grandma.png',imageSources+'grandma.png');
-Game.Loader.Replace('grandma2.png',imageSources+'grandma.png');
-Game.Loader.Replace('witchGrandma.png',imageSources+'grandma.png');
-Game.Loader.Replace('alternateGrandma.png',imageSources+'grandma.png');
-Game.Loader.Replace('antiGrandma.png',imageSources+'grandma.png');
-Game.Loader.Replace('transmutedGrandma.png',imageSources+'grandma.png');
-Game.Loader.Replace('templeGrandma.png',imageSources+'grandma.png');
-Game.Loader.Replace('workerGrandma.png',imageSources+'grandma.png');
-Game.Loader.Replace('minerGrandma.png',imageSources+'grandma.png');
-Game.Loader.Replace('bankGrandma.png',imageSources+'grandma.png');
-Game.Loader.Replace('bunnyGrandma.png',imageSources+'grandma.png');
-Game.Loader.Replace('cosmicGrandma.png',imageSources+'grandma.png');
-Game.Loader.Replace('farmerGrandma.png',imageSources+'grandma.png');
-Game.Loader.Replace('elfGrandma.png',imageSources+'grandma.png');
-Game.Loader.Replace('alteredGrandma.png',imageSources+'grandma.png');
-Game.Loader.Replace('cosmicGrandma.png',imageSources+'grandma.png');
-Game.Loader.Replace('emetaGrandma.png',imageSources+'grandma.png');
-Game.Loader.Replace('cloneGrandma.png',imageSources+'grandma.png');
+
+// for each grandma in the array, replace the image with the new image
+for (var i = 0; i < grandmas.length; i++) {
+	Game.Loader.Replace(grandmas[i]+'.png',imageSources+'grandma.png');
+};
