8dcc84ddf459b4512057a3de1e6dedeea68d5b04
Paul Montague <paul.montague@eraneos.com>
2024-06-12 02:13:02 +0200

    :3

---
+++ boykisser.js
@@ -0,0 +1,25 @@
+
+var imageSources = "https://kayofeld.github.io/Cookie-Clicker-mod/"
+
+Game.Loader.Replace('buildings.png',imageSources+'buildingss.png');
+Game.Loader.Replace('perfectCookie.png',imageSources+'boykissser.png');
+Game.Loader.Replace('cursor.png',imageSources+'cursor.png');
+Game.Loader.Replace('goldCookie.png',imageSources+'kawaiiCookie.png');
+Game.Loader.Replace('grandma.png',imageSources+'grandma.png');
+Game.Loader.Replace('grandma2.png',imageSources+'grandma.png');
+Game.Loader.Replace('witchGrandma.png',imageSources+'grandma.png');
+Game.Loader.Replace('alternateGrandma.png',imageSources+'grandma.png');
+Game.Loader.Replace('antiGrandma.png',imageSources+'grandma.png');
+Game.Loader.Replace('transmutedGrandma.png',imageSources+'grandma.png');
+Game.Loader.Replace('templeGrandma.png',imageSources+'grandma.png');
+Game.Loader.Replace('workerGrandma.png',imageSources+'grandma.png');
+Game.Loader.Replace('minerGrandma.png',imageSources+'grandma.png');
+Game.Loader.Replace('bankGrandma.png',imageSources+'grandma.png');
+Game.Loader.Replace('bunnyGrandma.png',imageSources+'grandma.png');
+Game.Loader.Replace('cosmicGrandma.png',imageSources+'grandma.png');
+Game.Loader.Replace('farmerGrandma.png',imageSources+'grandma.png');
+Game.Loader.Replace('elfGrandma.png',imageSources+'grandma.png');
+Game.Loader.Replace('alteredGrandma.png',imageSources+'grandma.png');
+Game.Loader.Replace('cosmicGrandma.png',imageSources+'grandma.png');
+Game.Loader.Replace('emetaGrandma.png',imageSources+'grandma.png');
+Game.Loader.Replace('cloneGrandma.png',imageSources+'grandma.png');
